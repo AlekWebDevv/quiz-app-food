6cc2b512aef67f16734b659fcb2278f7a3008159
AlekWebDevv <bykalex300@gmail.com>
2020-11-26 00:27:20 -0800

    Added a wire frame to HTML and CSS

---
+++ app.js
@@ -0,0 +1,29 @@
+const STORE = {
+    questions: [
+        {
+            question: 'What color is broccoli?',
+            answers: ['red','orange','pink','green'],
+            correctAnswer: 'green'
+        },
+        { 
+            question: 'How many scolville Heat Units does Carolina Reaper the hottest pepper have?',
+            answers: ['8,820,000 SHU','3,180,000 SHU','1,300,000 SHU','730,000 SHU'],
+            correctAnswer: '2,200,000 SHU' 
+        },
+        { 
+            question: 'Which one of these is NOT a major food group?',
+            answers: ['Protein','Vegetables','Chocalate','Grains'],
+            correctanswer: 'Chocalate'
+        },
+        { 
+            question: 'Which one of these is not an ingrediant in cake?',
+            answers: ['Eggs','Beef','Flour','Milk'],
+            correctAnswer: 'Beef'
+        },
+        { 
+            question: 'Which one of these is tea NOT made of?',
+            answers: ['Roots','Bark','Beans','Leaves',],
+            correctAnswer: 'Leaves'
+        },
+    ],
+}
